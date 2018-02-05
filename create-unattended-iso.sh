1b604aafe9e61f63a14a59399ddfc82497ecf2a8
Viktor Bergqvist <vb@paspx.com>
2018-02-05 16:34:08 +0100

    fixed preseed

--- create-unattended-iso.sh
+++ create-unattended-iso.sh
@@ -172,7 +172,11 @@ sed -i -r 's/timeout\s+[0-9]+/timeout 1/g' $tmp/iso_new/isolinux/isolinux.cfg
 if [ $ub1604 == "yes" ]; then
    late_command="apt-install wget; in-target wget --no-check-certificate -O /home/$username/start.sh https://raw.githubusercontent.com/PASPX/ubuntu16/master/start.sh ;\
      in-target chmod +x /home/$username/start.sh ;"
+else 
+   late_command="chroot /target wget -O /home/$username/start.sh https://raw.githubusercontent.com/PASPX/ubuntu16/master/start.sh ;\
+     chroot /target chmod +x /home/$username/start.sh ;"
 fi
+
 
 
 
